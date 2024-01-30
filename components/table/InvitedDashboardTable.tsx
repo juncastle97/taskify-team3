6c517fb59042e47e2ee70cb03dcf51c14dc54198
CheeseB <43297823+CheeseB@users.noreply.github.com>
2024-01-30 11:04:49 +0900

    [내 대시보드] 초대받은 대시보드 컴포넌트 구현 (#20)
    * refactor: 컨벤션에 따라서 스타일 이름 수정
    
    * feat: 초대받은 대시보드 테이블에 사용할 검색창 컴포넌트 구현
    
    * feat: 테이블 스타일 수정, hydration 오류 해결

--- components/table/InvitedDashboardTable.tsx
+++ components/table/InvitedDashboardTable.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./InvitedDashboardTable.module.scss";
 import { MappedInvitations } from "@/types/invitations";
 import noInvitationIcon from "@/public/icons/noInvitation.svg";
+import SearchBar from "@/components/input/SearchBar";
 
 const NO_INVITATION_ICON_SIZE = 100;
 
@@ -12,7 +13,7 @@ interface InvitedDashboardTableProps {
 
 const NoInvitation = () => {
   return (
-    <div className={styles["no-invitation"]}>
+    <div className={styles.noInvitation}>
       <Image
         src={noInvitationIcon}
         alt="No Invitation"
@@ -32,15 +33,28 @@ const InvitedDashboardTable = ({
     <div className={styles.wrapper}>
       <div className={styles.title}>초대받은 대시보드</div>
       {totalCount ? (
-        <ul>
-          {invitations.map(invitation => (
-            <li>
-              {invitation.dashboard}
-              {invitation.inviter}
-              {invitation.inviteAccepted}
-            </li>
-          ))}
-        </ul>
+        <div className={styles.tableArea}>
+          <SearchBar />
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>이름</th>
+                <th>초대자</th>
+                <th>수락 여부</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invitations.map(invitation => (
+                /** @TODO 수락/거절 버튼 컴포넌트 완성되면 적용하기 */
+                <tr>
+                  <td>{invitation.dashboard}</td>
+                  <td>{invitation.inviter}</td>
+                  <td>{invitation.inviteAccepted ? "수락" : "거절"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       ) : (
         <NoInvitation />
       )}
